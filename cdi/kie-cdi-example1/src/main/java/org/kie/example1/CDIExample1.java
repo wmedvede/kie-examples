fcb146c6042df648da0c79ac367faa2b18b93518
Mark Proctor <mproctor@codehaus.org>
2012-12-07 05:11:18 +0000

    added exec:exec

--- cdi/kie-cdi-example1/src/main/java/org/kie/example1/CDIExample1.java
+++ cdi/kie-cdi-example1/src/main/java/org/kie/example1/CDIExample1.java
@@ -1,13 +1,9 @@
 package org.kie.example1;
 
-import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 
-import org.drools.cdi.example.TestClass;
-import org.jboss.weld.environment.se.StartMain;
 import org.jboss.weld.environment.se.Weld;
 import org.jboss.weld.environment.se.WeldContainer;
-import org.jboss.weld.environment.se.events.ContainerInitialized;
 import org.kie.cdi.KSession;
 import org.kie.runtime.KieSession;
 
