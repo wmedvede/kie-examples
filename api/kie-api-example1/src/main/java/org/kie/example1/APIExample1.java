4bc9b94f92abc3781cc1598fe3bc4a5a67ec239b
Mark Proctor <mproctor@codehaus.org>
2012-12-12 11:48:53 +0000

    Added unit test for APIExample1

--- api/kie-api-example1/src/main/java/org/kie/example1/APIExample1.java
+++ api/kie-api-example1/src/main/java/org/kie/example1/APIExample1.java
@@ -1,5 +1,7 @@
 package org.kie.example1;
 
+import java.io.PrintStream;
+
 import org.kie.builder.KieContainer;
 import org.kie.builder.KieServices;
 import org.kie.runtime.KieSession;
@@ -10,12 +12,19 @@ import org.kie.runtime.KieSession;
  */
 public class APIExample1  {
 
-    public static void main( String[] args ) {
+    
+    public void go(PrintStream out) {
         KieServices ks = KieServices.Factory.get();        
         KieContainer kContainer = ks.newKieClasspathContainer();
         KieSession kSession = kContainer.newKieSession( "ksession1" );
+        kSession.setGlobal( "out", out );
         kSession.insert( new Message("Dave", "Hello, HAL. Do you read me, HAL?") );
         kSession.fireAllRules();
+    }    
+    
+    
+    public static void main( String[] args ) {
+        new APIExample1().go( System.out );
     }
 
 }
